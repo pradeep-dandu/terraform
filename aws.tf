d051378dc22586477b9100391deb3350e6dd2098
pradeep-dandu <108711273+pradeep-dandu@users.noreply.github.com>
2022-08-04 11:17:01 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "pradeep-web" {
-    ami = "ami-08df646e18b182346"
+    ami = "ami-076e3a557efe1aa9c"
     instance_type = "t2.micro"
     count = 1
     subnet_id = "subnet-0d47c7cc17ec8f644"
